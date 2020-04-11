3d251557cf25ddf3de3f59688df7c2094ab66590
weixinwang <weixinwang442@gmail.com>
2020-04-10 22:24:04 -0400

    2nd deriv from 1st deriv in degenerated cases.

--- pdf_MF_normal_deriv.m
+++ pdf_MF_normal_deriv.m
@@ -53,36 +53,58 @@ if ~bool_scaled
     
     if bool_ddc
         % compute the second order derivatives of the normalizing constant
-        ddc=zeros(3,3);
+        c = pdf_MF_normal(s);
+        A = zeros(9,9);
+        b = zeros(9,1);
         
-        if length(unique(s))==3
-            % if s are different, use the first order derivative to
-            % calculate the second order derivative
-            c = pdf_MF_normal(s);
-            for i=1:3
-                jk = setdiff(1:3,i);
-                j = jk(1);
-                k = jk(2);
-                ddcdTij = -dc(i)*s(i)/(s(j)^2-s(i)^2) + dc(j)*s(j)/(s(j)^2-s(i)^2);
-                ddcdTik = -dc(i)*s(i)/(s(k)^2-s(i)^2) + dc(k)*s(k)/(s(k)^2-s(i)^2);
-                ddc(i,i) = c-ddcdTij-ddcdTik;
-                for j=i+1:3
-                    k = setdiff(1:3,[i,j]);
-                    dcdTkk = dc(k);
-                    ddcdTij = -dc(i)*s(j)/(s(j)^2-s(i)^2) + dc(j)*s(i)/(s(j)^2-s(i)^2);
-                    ddc(i,j) = dcdTkk+ddcdTij;
-                    ddc(j,i) = ddc(i,j);
-                end
-            end
-        else
-            for i=1:3
-                ddc(i,i) = integral(@(u) f_kunze_s_deriv_ii(u,s,i),-1,1);
-                for j=i+1:3
-                    ddc(i,j) = integral(@(u) f_kunze_s_deriv_ij(u,s,i,j),-1,1);
-                    ddc(j,i)=ddc(i,j);
+        for i = 1:3
+            for j = 1:3
+                k = setdiff(1:3,[i,j]);
+                if i==j
+                    if s(i)~=s(k(1)) && s(i)~=s(k(2))
+                        A(3*(i-1)+j,3*(i-1)+j) = 1;
+                        b(3*(i-1)+j) = c-(-dc(i)*s(i)+dc(k(1))*s(k(1)))/(s(k(1))^2-s(i)^2)-(-dc(i)*s(i)+dc(k(2))*s(k(2)))/(s(k(2))^2-s(i)^2);
+                    elseif s(i)~=s(k(1)) && s(i)==s(k(2)) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 3/2;
+                        A(3*(i-1)+j,3*(i-1)+k(2)) = -1/2;
+                        b(3*(i-1)+j) = c-(-dc(i)*s(i)+dc(k(1))*s(k(1)))/(s(k(1))^2-s(i)^2)-dc(i)/2/s(i);
+                    elseif s(i)~=s(k(1)) && s(i)==s(k(2)) && s(i)==0
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        b(3*(i-1)+j) = c-(-dc(i)*s(i)+dc(k(1))*s(k(1)))/(s(k(1))^2-s(i)^2);
+                    elseif s(i)==s(k(1)) && s(i)~=s(k(2)) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 3/2;
+                        A(3*(i-1)+j,3*(i-1)+k(1)) = -1/2;
+                        b(3*(i-1)+j) = c-dc(i)/2/s(i)-(-dc(i)*s(i)+dc(k(2))*s(k(2)))/(s(k(2))^2-s(i)^2);
+                    elseif s(i)==s(k(1)) && s(i)==s(k(2)) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        A(3*(i-1)+j,3*(i-1)+k(1)) = -1/2;
+                        A(3*(i-1)+j,3*(i-1)+k(2)) = -1/2;
+                        b(3*(i-1)+j) = c-dc(i)/s(i);
+                    elseif s(i)==s(k(1)) && s(i)~=s(k(2)) && s(i)==0
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        b(3*(i-1)+j) = c-(-dc(i)*s(i)+dc(k(2))*s(k(2)))/(s(k(2))^2-s(i)^2);
+                    else
+                        A(3*(i-1)+j,3*(i-1)+j) = 3;
+                        b(3*(i-1)+j)=c;
+                    end
+                else
+                    if s(i)~=s(j)
+                        A(3*(i-1)+j,3*(i-1)+j) = 1;
+                        b(3*(i-1)+j) = dc(k)+(-dc(i)*s(j)+dc(j)*s(i))/(s(j)^2-s(i)^2);
+                    elseif s(i)==s(j) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 3/2;
+                        A(3*(i-1)+j,3*(i-1)+i) = -1/2;
+                        b(3*(i-1)+j) = dc(k)-dc(i)/2/s(i);
+                    else
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        b(3*(i-1)+j) = dc(k);
+                    end
                 end
             end
         end
+        
+        ddc = A\b;
+        ddc = [ddc(1:3),ddc(4:6),ddc(7:9)];
         
         varargout{2}=ddc;
     end
@@ -103,43 +125,65 @@ else
     % compute the second order derivatives of the scaled normalizing
     % constant
     if bool_ddc
-        ddc_bar=zeros(3,3);
+        c_bar = pdf_MF_normal(s,1);
+        A = zeros(9,9);
+        b = zeros(9,1);
         
-        if length(unique(s))==3
-            % if s are different, use the first order derivative to
-            % calculate the second order derivative
-            c_bar = pdf_MF_normal(s,1);
-            for i=1:3
-                jk = setdiff(1:3,i);
-                j = jk(1);
-                k = jk(2);
-                EQij = -(c_bar+dc_bar(i))*s(i)/(s(j)^2-s(i)^2) + ...
-                    (c_bar+dc_bar(j))*s(j)/(s(j)^2-s(i)^2);
-                EQik = -(c_bar+dc_bar(i))*s(i)/(s(k)^2-s(i)^2) + ...
-                    (c_bar+dc_bar(k))*s(k)/(s(k)^2-s(i)^2);
-                ddc_bar(i,i) = -2*dc_bar(i)-EQij-EQik;
-                for j=i+1:3
-                    k = setdiff(1:3,[i,j]);
-                    EQij = -(c_bar+dc_bar(i))*s(j)/(s(j)^2-s(i)^2) + ...
-                        (c_bar+dc_bar(j))*s(i)/(s(j)^2-s(i)^2);
-                    EQkk = c_bar+dc_bar(k);
-                    ddc_bar(i,j) = -c_bar-dc_bar(i)-dc_bar(j)+EQkk+EQij;
-                    ddc_bar(j,i) = ddc_bar(i,j);
+        for i = 1:3
+            for j = 1:3
+                k = setdiff(1:3,[i,j]);
+                if i==j
+                    if s(i)~=s(k(1)) && s(i)~=s(k(2))
+                        A(3*(i-1)+j,3*(i-1)+j) = 1;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar/(s(i)+s(k(1)))+dc_bar(i)*s(i)/(s(k(1))^2-s(i)^2)-dc_bar(k(1))*s(k(1))/(s(k(1))^2-s(i)^2)...
+                            - c_bar/(s(i)+s(k(2)))+dc_bar(i)*s(i)/(s(k(2))^2-s(i)^2)-dc_bar(k(2))*s(k(2))/(s(k(2))^2-s(i)^2);
+                    elseif s(i)~=s(k(1)) && s(i)==s(k(2)) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 3/2;
+                        A(3*(i-1)+j,3*(i-1)+k(2)) = -1/2;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar/(s(i)+s(k(1)))+dc_bar(i)*s(i)/(s(k(1))^2-s(i)^2)-dc_bar(k(1))*s(k(1))/(s(k(1))^2-s(i)^2)...
+                            - c_bar/2/s(i)-(1/2/s(i)+1/2)*dc_bar(i)+dc_bar(k(2))/2;
+                    elseif s(i)~=s(k(1)) && s(i)==s(k(2)) && s(i)==0
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar/(s(i)+s(k(1)))+dc_bar(i)*s(i)/(s(k(1))^2-s(i)^2)-dc_bar(k(1))*s(k(1))/(s(k(1))^2-s(i)^2)...
+                            - c_bar-2*dc_bar(i);
+                    elseif s(i)==s(k(1)) && s(i)~=s(k(2)) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 3/2;
+                        A(3*(i-1)+j,3*(i-1)+k(1)) = -1/2;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar/2/s(i)-(1/2/s(i)+1/2)*dc_bar(i)+dc_bar(k(1))/2 ...
+                            - c_bar/(s(i)+s(k(2)))+dc_bar(i)*s(i)/(s(k(2))^2-s(i)^2)-dc_bar(k(2))*s(k(2))/(s(k(2))^2-s(i)^2);
+                    elseif s(i)==s(k(1)) && s(i)==s(k(2)) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        A(3*(i-1)+j,3*(i-1)+k(1)) = -1/2;
+                        A(3*(i-1)+j,3*(i-1)+k(2)) = -1/2;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar/2/s(i)-(1/2/s(i)+1/2)*dc_bar(i)+dc_bar(k(1))/2 ...
+                            - c_bar/2/s(i)-(1/2/s(i)+1/2)*dc_bar(i)+dc_bar(k(2))/2;
+                    elseif s(i)==s(k(1)) && s(i)~=s(k(2)) && s(i)==0
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar-2*dc_bar(i)...
+                            - c_bar/(s(i)+s(k(2)))+dc_bar(i)*s(i)/(s(k(2))^2-s(i)^2)-dc_bar(k(2))*s(k(2))/(s(k(2))^2-s(i)^2);
+                    else
+                        A(3*(i-1)+j,3*(i-1)+j) = 3;
+                        b(3*(i-1)+j) = -2*dc_bar(i) - c_bar-2*dc_bar(i) - c_bar-2*dc_bar(i);
+                    end
+                else
+                    if s(i)~=s(j)
+                        A(3*(i-1)+j,3*(i-1)+j) = 1;
+                        b(3*(i-1)+j) = -c_bar/(s(i)+s(j)) - (1+s(j)/(s(j)^2-s(i)^2))*dc_bar(i)...
+                            - (1-s(i)/(s(j)^2-s(i)^2))*dc_bar(j) + dc_bar(k);
+                    elseif s(i)==s(j) && s(i)~=0
+                        A(3*(i-1)+j,3*(i-1)+j) = 3/2;
+                        A(3*(i-1)+j,3*(i-1)+i) = -1/2;
+                        b(3*(i-1)+j) = -c_bar/2/s(i) - (1/2+1/2/s(i))*dc_bar(i) - 3/2*dc_bar(j) + dc_bar(k);
+                    else
+                        A(3*(i-1)+j,3*(i-1)+j) = 2;
+                        b(3*(i-1)+j) = -c_bar - 2*dc_bar(i) - 2*dc_bar(j) + dc_bar(k);
+                    end
                 end
             end
-        else
-            for i=1:3
-                index=circshift([1 2 3],[0 4-i]);
-                j=index(2);
-                k=index(3);
-
-                c_bar=pdf_MF_normal(s,1);
-                ddc_bar(k,k) = integral(@(u) f_kunze_s_deriv_scaled_kk(u,[s(i),s(j),s(k)]),-1,1);
-                ddc_bar(i,j) = integral(@(u) f_kunze_s_deriv_scaled_ij(u,[s(i),s(j),s(k)]),-1,1) ...
-                    -dc_bar(i)-dc_bar(j)-c_bar;
-                ddc_bar(j,i) = ddc_bar(i,j);
-            end
         end
+        
+        ddc_bar = A\b;
+        ddc_bar = [ddc_bar(1:3),ddc_bar(4:6),ddc_bar(7:9)];
         
         varargout{2}=ddc_bar;
     end
@@ -155,25 +199,6 @@ Y=1/2*J.*(u-1).*exp((min([s(1) s(2)])+s(3))*(u-1));
 end
 
 
-function Y=f_kunze_s_deriv_scaled_kk(u,s)
-% integrand for the second order derivative of the scaled normalizing constant
-
-J=besseli(0,1/2*(s(1)-s(2))*(1-u),1).*besseli(0,1/2*(s(1)+s(2))*(1+u),1);
-Y=1/2*J.*(u-1).^2.*exp((min([s(1) s(2)])+s(3))*(u-1));
-
-end
-
-function Y=f_kunze_s_deriv_scaled_ij(u,s)
-% integrand for the scaled second order derivative of the normalizing constant
-
-J=1/4*besseli(1,1/2*(s(2)-s(3))*(1-u),1).*besseli(0,1/2*(s(2)+s(3))*(1+u),1) ...
-    .*exp((s(1)+min([s(2) s(3)]))*(u-1)).*u.*(1-u) ...
-    +1/4*besseli(0,1/2*(s(2)-s(3))*(1-u),1).*besseli(1,1/2*(s(2)+s(3))*(1+u),1) ...
-    .*exp((s(1)+min([s(2) s(3)]))*(u-1)).*u.*(1+u);
-Y=J;
-
-end
-
 function Y=f_kunze_s_deriv_i(u,s,i)
 % integrand for the derivative of the normalizing constant
 index=circshift([1 2 3],[0 4-i]);
@@ -182,26 +207,5 @@ k=index(3);
 
 J00=besseli(0,1/2*(s(j)-s(k))*(1-u)).*besseli(0,1/2*(s(j)+s(k))*(1+u));
 Y=1/2*J00.*u.*exp(s(i)*u);
-
-end
-
-function Y=f_kunze_s_deriv_ii(u,s,i)
-% integrand for the second-order derivative of the normalizing constant
-index=circshift([1 2 3],[0 4-i]);
-j=index(2);
-k=index(3);
-
-J00=besseli(0,1/2*(s(j)-s(k))*(1-u)).*besseli(0,1/2*(s(j)+s(k))*(1+u));
-Y=1/2*J00.*u.^2.*exp(s(i)*u);
-
-end
-
-function Y=f_kunze_s_deriv_ij(u,s,i,j)
-% integrand for the mixed second-order derivative of the normalizing constant
-k=setdiff([1 2 3],[i,j]);
-
-J10=besseli(1,1/2*(s(j)-s(k))*(1-u)).*besseli(0,1/2*(s(j)+s(k))*(1+u));
-J01=besseli(0,1/2*(s(j)-s(k))*(1-u)).*besseli(1,1/2*(s(j)+s(k))*(1+u));
-Y=1/4*J10.*u.*(1-u).*exp(s(i)*u) + 1/4*J01.*u.*(1+u).*exp(s(i)*u);
 
 end
